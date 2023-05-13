6d3b94e92bff3fcc60d44c433dc8f976c4aa97ce
Roshan Thapa <roshan@Roshans-MacBook-Air.local>
2023-05-13 15:27:23 +0545

    first commit

---
+++ web_scrapper.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_price():
+    origin_url="https://merolagani.com/CompanyDetail.aspx?symbol="
+    scripts_name=["sahas","dordi","hhl","jalpa"]
+    scripts_tol=[500,250,200,1900]
+
+    for index,script in enumerate(scripts_name):
+        target_url=origin_url+script
+        # print(target_url)
+
+        web_resp=requests.get(target_url)
+
+        if web_resp.status_code==200:
+            # print("Successfully response gone and opened file!")
+
+            soup=BeautifulSoup(web_resp.text,'html.parser')
+
+            strong_content=soup.find("strong")
+            strong_content=str(strong_content)
+            # print(strong_content)
+
+            first_index=strong_content.find("\">")+2
+            last_index=strong_content.find("</span>")
+
+            price_content=strong_content[first_index:last_index]
+
+            price_string=str(price_content)
+
+            # replacing if it has , for thousand places
+            if price_string.find(",")==-1:
+                price_string=price_string
+            else:
+                price_string=price_string.replace(",","")
+
+            price_int=int(float(price_string))
+            # print(price_int)
+
+            # checking if the current price is below tol
+            if price_int<=scripts_tol[index]:
+                print("Current Market Price is below the tol price mentioned")
+            else:
+                print("Market price is high")
+
+            print(f'{price_int} - Mp and {scripts_tol[index]} -  tolerance :::-> {script}')
+
+get_price()
